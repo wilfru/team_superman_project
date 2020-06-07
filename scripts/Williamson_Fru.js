4bb5f7e0a1a084c5fd98e059816c6be9d502cfaa
wilfru <fruwilliams@gmail.com>
2020-06-07 09:46:50 +0300

    myscript_wilfru

---
+++ scripts/Williamson_Fru.js
@@ -0,0 +1,7 @@
+function wil(fullname="Williamson Fru", id ="HNG-06733", email ="fruwilliams@gmail.com", language ="JavaScript"){
+    console.log(
+     `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`
+    );
+}
+wil();
+
--- scripts/rolyteck.php
+++ scripts/rolyteck.php
@@ -1,11 +1,12 @@
+
 <?php
-$name = "Roland Chukwudi Blessing ";
 
-$hng_id = "HNG-02628";
+ //function to display intern details
+ function intern($name, $id, $pro_lang, $email)
+ {
+     return "Hello World, this is ".$name. " with HNGi7 id ".$id. " using ".$pro_lang." for stage 2 task.".$email;
+ }
 
-$language = "PHP";
+ $output = intern("Roland Blessing", "HNG-02628","PHP","Rolyconsult@gmail.com");
 
-$email = "Rolyconsult@gmail.com";
-
-
-echo "Hello World, this is {$name} with HNGi7 ID {$hng_id} using {$language} for stage 2 task.{$email}";
+ echo $output;
